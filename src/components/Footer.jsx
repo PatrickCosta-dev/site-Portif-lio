c9fa12ca0d512aaff57acf81fcbb26857999f09d
Patrick Costa <91685943+PatrickCosta-dev@users.noreply.github.com>
2025-06-29 21:22:32 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-500">
         <p className="mb-2">
-          © 2025 Patrick Costa.
+          © 2025 All rights reserved.
         </p>
       </div>
     </footer>
