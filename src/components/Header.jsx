74511bbb4744ee8077f27904a0b98fc478e99cf8
Patrick Costa <patrickcosta20011975@gmail.com>
2025-06-30 20:14:52 -0300

    Remoção do botão de alteração de tema

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,12 +41,8 @@ const Header = () => {
             </motion.h1>
 
             {/* Botão de Tema */}
-            <motion.div initial={{ opacity: 0, x: 20 }} animate={{ opacity: 1, x: 0 }}>
-              <Button variant="ghost" size="icon" onClick={handleThemeClick}>
-                <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
-              </Button>
+            <motion.div initial={{ opacity: 0, x: 20 }} animate={{ opacity: 0, x: 0 }}>
+              
             </motion.div>
           </div>
         </div>
