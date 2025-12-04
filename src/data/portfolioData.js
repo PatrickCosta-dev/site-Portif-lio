431334dd98ef6d802d6d652d163d94a7da5e4e30
Patrick Costa <91685943+PatrickCosta-dev@users.noreply.github.com>
2025-12-04 20:33:24 -0300

    Update portfolioData.js

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -3,8 +3,8 @@ import { Code, Smartphone, Server, Database, Cloud, Workflow, BrainCircuit, Orbi
 export const skills = [
   { name: 'Python', icon:'https://img.icons8.com/?size=96&id=13441&format=png'},
   { name: 'C++', icon:'https://img.icons8.com/?size=100&id=TpULddJc4gTh&format=png&color=000000' },
-  { name: 'Node.js', icon: 'https://img.icons8.com/?size=96&id=hsPbhkOH4FMe&format=png' },
   { name: 'React', icon: 'https://img.icons8.com/?size=100&id=35989&format=png' },
+  { name: 'Node.js', icon: 'https://img.icons8.com/?size=96&id=hsPbhkOH4FMe&format=png' },
   { name: 'SQL', icon: 'https://www.ibm.com/adobe/dynamicmedia/deliver/dm-aid--f385b240-1636-41a3-97a6-a0a6fbc58352/azure-sql-server-monitoring.png?quality=90&preferwebp=true' },
   { name: 'AWS', icon: 'https://img.icons8.com/?size=96&id=33039&format=png' },
   { name: 'N8N', icon: 'https://upload.wikimedia.org/wikipedia/commons/5/53/N8n-logo-new.svg' },
